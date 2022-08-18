5dcbccbfe5fbbf099aaadda05245782ec10293d7
Deyvid Tavares <devdeyvid2022@gmail.com>
2022-08-18 17:11:46 -0300

    adicionando um context api

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,25 @@
+import { useContext, useEffect } from "react"
+import { CartContext } from "../contexts/CartContext"
+
 export function Cart(){
+    const { cart, setCart, valores, setValores, soma, setSoma, cartDelete,valorTotal } = useContext(CartContext)
+    useEffect(()=>valorTotal(),[valores])
     return (
-        <div className="text-xl text-white font-bold font arial w-screen h-5/6 flex items-center justify-center ">
-            <p className="shadow-2xl shadow-zinc-800">ESSA SESSAO FUNCIONARÁ EM BREVE</p>
-        </div>
+        
+        <ul className="flex flex-col gap-5 w-72 mt-5 ">
+        {cart.map((cart, i) => {
+          return (
+            <li className="bg-white shadow-2xl shadow-zinc-800" key={i} >{cart.nome}
+              <div className="flex justify-between">
+                <img className="w-10 h-10" src={cart.img} alt="" />
+                <button onClick={() => cartDelete(i)} className="bg-red-400 w-6 h-6">x</button>
+              </div>
+
+            </li>
+          )
+        })}
+        <p className={`${soma == 0 ?'hidden' :'flex'} bg-green-400 p-2 rounded-md`}>Valor total: R${soma}</p>
+        <p className={`${soma == 0 ?'flex' :'hidden'} bg-white rounded-md`}>Seu carrinho ainda está vazio, compre algo na loja</p>
+      </ul>
     )
 }
--- src/components/Loja.jsx
+++ src/components/Loja.jsx
@@ -1,56 +1,29 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { CartContext } from "../contexts/CartContext"
 import { produtos } from "../Products"
 
+
 export function Loja() {
-  let [soma, setSoma] = useState(0)
-  const [valores, setValores] = useState([])
-  const [cart, setCart] = useState([])
 
 
-  
-  function cartDelete(i) {
-    const tempCart = [...cart]
-    tempCart.splice(i, 1)
-    setCart(tempCart)
-    const tempValores = [...valores]
-    tempValores.splice(i, 1)
-    setValores(tempValores)
-  }
-
+  const { cart, setCart, valores, setValores, soma, setSoma,valorTotal } = useContext(CartContext)
   
   useEffect(()=>valorTotal(),[valores])
 
-  function valorTotal() {
-    var numeros = [...valores];
-    var total = numeros.reduce(function(total, numero){
-    return total + numero;
-    }, 0);
-    console.log(total)
-    setSoma(total);
-  }
+  
+  
   function addItem(produto) {
     setCart([...cart, produto])
     setValores([...valores, produto.Preco])
   }
   return (
-    <ul className="flex flex-wrap gap-4 justify-center mt-4">
-      {produtos.map((produto, i) => {
-        return (<li className="shadow-zinc-700 bg-white shadow-2xl rounded-md p-2" key={i}><img className="w-64 rounded-md h-80 " src={produto.img} alt="" /><p>{produto.nome}</p><p>Preço: <span className="text-blue-700">R${produto.Preco}</span></p><button onClick={() => addItem(produto)}>adicionar ao carrinho</button></li>)
-      })}
-      <ul>
-        {cart.map((cart, i) => {
-          return (
-            <li key={i} >{cart.nome}
-              <div className="flex justify-between">
-                <img className="w-10 h-10" src={cart.img} alt="" />
-                <button onClick={() => cartDelete(i)} className="bg-red-400 w-6 h-6">x</button>
-              </div>
 
-            </li>
-          )
-        })}
-        <p>{soma}</p>
-      </ul>
+    <ul className="flex flex-wrap gap-4 justify-center mt-4">
+      {/* <p className="absolute bg-green-400 text-white rounded-md w-96 h-12 flex items-center justify-center">Item adicionado ao carrinho</p> */}
+      {produtos.map((produto, i) => {
+        return (<li className="shadow-zinc-700 bg-white shadow-2xl rounded-md p-2" key={i}><img className="w-64 rounded-md h-80 " src={produto.img} alt="" /><p>{produto.nome}</p><p>Preço: <span className="text-blue-700">R${produto.Preco}</span></p><button className="p-2 shadow-zinc-700 shadow-md rounded-md text-white bg-violet-800 " onClick={() => addItem(produto)}>adicionar ao carrinho</button></li>)
+      })}
+
     </ul>
   )
 }
