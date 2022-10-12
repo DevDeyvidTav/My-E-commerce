cdf296f0945eba1d0f8df85fe88106ab00b085b3
Deyvid Tavares <devdeyvid2022@gmail.com>
2022-10-12 09:33:19 -0300

    add-contacts and icons react-icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,14 @@
-import { useContext } from "react"
-import { CartContext } from "../contexts/CartContext"
-import Wpp from '../images/Whatsapp_icon-icons.com_66931.svg'
-import mario from '../images/mario.png'
+import { BsGithub } from 'react-icons/bs'
+import {AiFillLinkedin, AiOutlineWhatsApp} from 'react-icons/ai'
 
 export function Contact(){
     return (
-        <div className="w-full h-5/6 justify-center flex flex-wrap mt-20 text-white font-bold text-2xl font-arial gap-5 md:gap-44 absolute">
+        <div className="w-full h-5/6 justify-center items-center flex flex-wrap mt-20 text-white font-bold text-2xl font-arial gap-5 md:gap-44 absolute">
             <div className=" flex flex-col w-72 items-center ">
-                <a href="https://wa.me/5581997210434" target="_blank"><button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300  flex items-center justify-center">WhatsApp<img className="w-7 h-7" src={Wpp} alt="" /></button></a>
-                <a href=""><button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300  flex items-center justify-center">E-mail</button></a>
-                <a href="https://www.linkedin.com/in/deyvid-tavares-37918b236/" target="_blank"> <button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300  flex items-center justify-center">LinkedIn</button></a>
-            </div>
-            <div className="flex flex-col gap-10">
-            <h2 className="text-3xl md:text-white text-violet-800">Mais Projetos</h2>
-                <div className="flex flex-wrap">
-                <img src={mario} className="w-44 h-32 cursor-pointer hover:w-56 hover:h-40 hover:duration-300 hover:shadow-2xl hover:shadow-white fixed" alt="" />
-                </div>
+                <a href="https://wa.me/5581997210434" target="_blank"><button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300 gap-3 flex items-center justify-center"><AiOutlineWhatsApp/> WhatsApp</button></a>
+                <a href=""><button onClick={(e) => e.preventDefault()} className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300  flex items-center justify-center">E-mail: devdeyvid2022@gmail.com</button></a>
+                <a href="https://www.linkedin.com/in/deyvid-tavares-37918b236/" target="_blank"> <button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300 gap-3  flex items-center justify-center"><AiFillLinkedin/>  LinkedIn</button></a>
+                <a href="https://github.com/DevDeyvidTav" target="_blank"> <button className="w-96 rounded-md hover:shadow-2xl hover:shadow-zinc-800 hover:bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 hover:duration-300 gap-3 flex items-center justify-center"><BsGithub/>  GitHub</button> </a>
             </div>
         </div>
     )
